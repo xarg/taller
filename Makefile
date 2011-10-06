acbd30f2ac2c616d5225d416886fda7d03e6dcad
Alexandru Plugaru <alexandru@gymglish.com>
2011-10-06 14:10:40 +0200

    Added a makefile, a few lines for begining.

---
+++ Makefile
@@ -0,0 +1,5 @@
+include $(GOROOT)/src/Make.inc
+TARG=github.com/humanfromearth/taller
+GOFILES=\
+	taller.go
+include $(GOROOT)/src/Make.pkg
---
+++ taller.go
@@ -0,0 +1,31 @@
+// taller package provides template libraries with flow controls as close to the native language as possible
+
+package taller
+
+const {
+	SECTION_RIGHT_DELIMITER = '['
+	SECTION_LEFT_DELIMITER = '['
+}
+
+type Template struct {
+	rdelimiter, ldelimiter []byte //default []
+	data []byte
+}
+
+type TemplateFile struct {
+	Read()
+}
+
+
+func NewTemplate() *Template {
+	template := new(Template)
+	template.
+	return Template
+}
+
+
+func (self *Template) Render() {
+
+}
+
+func 
