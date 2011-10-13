12f50d41a53a129815108292c45fad3ef12493bc
Alexandru Plugaru <alexandru.plugaru@gmail.com>
2011-10-13 16:02:27 +0200

    Created a sample .go template to be compiled

--- README.rst
+++ README.rst
@@ -11,3 +11,24 @@ A golang templating lib. Here are it's assertions:
 - I want to type less stuff and do more.
 - I want to include other templates in my template. (jinja?)
 - I want to extend templates.
+
+Implementation
+-----------------
+
+The rules are simple:
+
+0. There are no spaces in the declaration of tags such as: [ if x == 1]. 
+   Everything will begin with the actual tag: [if x[0] == 1]. 
+   There are no 2 tags on the same line. These are the only style enforcing rules.
+
+1. Find all [import ..], [extend ..] [include ..] and perform the 
+   required operations on them.
+
+2. Find all blocks [block ...] and [endblock] and replace them with actual 
+   golang function calls with the same names in the main loop. 
+   Create the functions.
+
+3. Find all [if ...][endif], [for ...][endfor] and just replace them as 
+   golang code. [if foo][endif] translates to if context["foo"] {} and so on.
+
+4. Replace all \`expression\` with fmt.Fprintf(output, expression)?
--- taller.go
+++ taller.go
@@ -1,9 +1,11 @@
-// taller package provides template libraries with flow controls as close to the native language as possible
+// taller package provides template libraries with flow controls as close to
+// the native language as possible
 
 package taller
 
 import (
-	//"fmt"
+	"bytes"
+//	"fmt"
 	"io/ioutil"
 )
 
@@ -25,6 +27,8 @@ type TemplateBytes struct {
 type TemplateFile struct {
 	filename string
 }
+
+type Context map[string] interface{}
 
 func NewTemplateBytes(data []byte) *TemplateBytes {
 	template := new(TemplateBytes)
@@ -52,8 +56,59 @@ func (self *TemplateFile) Content() []byte {
 
 func Render(template Template) []byte {
 	content := template.Content()
-	return content
+	parsed_content := Parse(content)
+	//compile and execute with the context
+	return parsed_content
 }
 
-func Parse(content []byte) {
+func Parse(content []byte) []byte {
+	output := []byte(`package taller
+import (
+	"fmt"
+	"bytes"
+	/*IMPORTS*/
+)
+/*BLOCKS*/
+func render(context Context) *bytes.Buffer {
+	output := bytes.NewBufferString("")
+	/*MAIN_BLOCK*/
+	return output
+}`)
+	blocks := map[string] *bytes.Buffer {
+		"IMPORTS":bytes.NewBufferString(""),
+		"BLOCKS":bytes.NewBufferString(""),
+		"MAIN_BLOCK":bytes.NewBufferString("")}
+	lines := bytes.Split(content, []byte("\n"))
+	for lineno, line := range lines {
+		//each line in the template will be parsed an translated to the
+		//appropriate block
+		block, parsed_line := ParseLine(lineno, line)
+		blocks[block].Write(parsed_line)
+	}
+	for block, parsed_lines := range blocks {
+		//replaces places such as /*MAIN_BLOCK*/ in the output using
+		//the parsed_lines
+		bytes.Replace(output, JoinBytes("/*", block, "*/"),
+			parsed_lines.Bytes(), -1)
+	}
+	return output
+}
+//give some string join them a single []byte seq
+func JoinBytes(string_list ... string) []byte {
+	var joined [][]byte
+	for _, str := range string_list {
+		joined = append(joined, []byte(str))
+	}
+	return bytes.Join(joined, []byte(""))
+}
+
+func ParseLine (lineno int, line []byte) (string, []byte) {
+	//On the first line should always be [extends "filename"]
+	var block_name string
+	var parsed_line []byte
+	if lineno == 0 {
+	}
+	block_name = "aaa"
+	parsed_line = []byte("aaa")
+	return block_name, parsed_line
 }
